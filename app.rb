3d128fabd2f93d856e0d58d6aa78bb31ee18119b
Moacir P. de Sá Pereira <moacir@gmail.com>
2016-08-30 19:31:39 -0400

    Further fight w/ encoding. Give up for now.

--- app.rb
+++ app.rb
@@ -4,15 +4,20 @@ require "httparty"
 
 class App < Sinatra::Base
 
+  # set :haml, format: :html5
+  # set :erb, layout_engint: :haml, layout: :layout
+  set :default_encoding, 'utf-8'
+
   get "/" do
     json = HTTParty.get("http://nywalker.newyorkscapes.org/books/let-the-great-world-spin-2009/geojson-instances").body
     instances = {}
+    specials = ["intro", "ciaran", "claire", "lara", "walker", "fernando", "the kid", "tillie", "solomon", "adelita", "gloria", "jaslyn"]
     # This causes an encoding error when it goes over to the erb.
-    instances_array = JSON.parse(json)["features"] # creates an array of instances
-    ["intro", "ciaran", "claire", "lara", "walker", "fernando", "the kid", "tillie", "solomon", "adelita", "gloria", "jaslyn"].each do |special|
-      instances[special] = instances_array.select{ |i| i["properties"]["special"] == special }.to_json
-    end
-    erb :index, locals: { geo_json: json, instances: instances }
+    # instances_array = JSON.parse(json)["features"] # creates an array of instances
+    # specials.each do |special|
+    #   instances[special] = instances_array.select{ |i| i["properties"]["special"] == special }.to_json
+    # end
+    erb :index, locals: { geo_json: json, instances: instances, specials: specials }
   end
 
 end
--- config.ru
+++ config.ru
@@ -1,3 +1,8 @@
+# encoding: utf-8
 require "./app"
+
+# these don't do anything.
+Encoding.default_external = 'utf-8'
+Encoding.default_internal = 'utf-8'
 
 run App
