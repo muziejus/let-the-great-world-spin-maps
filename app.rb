8a7fdf03cb232b79030c36826fb2691a1f67afc8
Moacir P. de Sá Pereira <moacir@gmail.com>
2016-08-30 17:38:25 -0400

    Just move this along.

--- app.rb
+++ app.rb
@@ -4,12 +4,12 @@ require "httparty"
 
 get "/" do
   json = HTTParty.get("http://nywalker.newyorkscapes.org/books/let-the-great-world-spin-2009/geojson-instances").body
+  instances = {}
   # This causes an encoding error when it goes over to the erb.
-  # instances_array = JSON.parse(json)["features"] # creates an array of instances
-  # instances = {}
-  # ["intro", "ciaran", "claire", "lara", "walker", "fernando", "the kid", "tillie", "solomon", "adelita", "gloria", "jaslyn"].each do |special|
-  #   instances[special] = instances_array.select{ |i| i["properties"]["special"] == special }.to_json
-  # end
-  erb :index, locals: { geo_json: json } #, instances: instances }
+  instances_array = JSON.parse(json)["features"] # creates an array of instances
+  ["intro", "ciaran", "claire", "lara", "walker", "fernando", "the kid", "tillie", "solomon", "adelita", "gloria", "jaslyn"].each do |special|
+    instances[special] = instances_array.select{ |i| i["properties"]["special"] == special }.to_json
+  end
+  erb :index, locals: { geo_json: json, instances: instances }
 end
 
--- public/app.js
+++ public/app.js
@@ -1,1 +1,30 @@
+function getSpecialPoints(points, specialText){
+   var returnPoints = [];
+   points.features.forEach(function(item, index){
+     if(item.properties.special === specialText){
+       returnPoints.push(item);
+     }
+   });
+  return returnPoints;
+};
+
+// Styles
+
+var geojsonMarkerOptions = {
+  radius: 8,
+  fillColor: "#adaada",
+  color: "#666",
+  weight: 1,
+  opacity: 0.8,
+  fillOpacity: 0.5
+};
+
+var averageMarkerOptions = {
+  radius: 4,
+  fillColor: "#7800ff",
+  color: "#666",
+  weight: 1,
+  opacity: 0.8,
+  fillOpacity: 0.5
+};
 
