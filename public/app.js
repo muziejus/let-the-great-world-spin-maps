072cb025a665695ea0c13606caf3b84cd9fa63ff
Moacir P. de Sá Pereira <moacir@gmail.com>
2016-08-30 19:32:41 -0400

    start abstracting out the javascript and even make it almost idiomatic.

--- public/app.js
+++ public/app.js
@@ -1,3 +1,31 @@
+function getAccumulatorPoints(points){
+  return {"type": "FeatureCollection", "features": getAccumulatorArray(points, "points")};
+};
+
+function getAccumulatorLine(points){
+  var pointsArray = getAccumulatorArray(points, "line");
+  return {"type": "LineString", "coordinates": pointsArray.map(function(item){ return item.geometry.coordinates })};
+};
+
+function getAccumulatorArray(points, option){
+  var step, centroid, pointsArray;
+  var centroidArray = [];
+  pointsArray = [points[0], points[1]]; // Because we start w/ step 3.
+  for(step = 2; step < points.length; step++){
+    pointsArray.push(points[step]);
+    centroid = turf.centroid({"type": "FeatureCollection", "features": pointsArray});
+    if(option === "points"){
+      centroid.popup = "Text: " + points[step].properties.text + "; page: " + points[step].properties.page;
+    };
+    centroidArray.push(centroid);
+  };
+  return centroidArray;
+};
+
+  
+
+
+
 function getSpecialPoints(points, specialText){
    var returnPoints = [];
    points.features.forEach(function(item, index){
