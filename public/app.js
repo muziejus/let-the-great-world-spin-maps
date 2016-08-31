0c9750f9e437a2a38d8e9c17d84f7e0b09f2acd1
Moacir P. de Sá Pereira <moacir@gmail.com>
2016-08-30 20:20:02 -0400

    JSLint things up a bit.

--- public/app.js
+++ public/app.js
@@ -1,35 +1,54 @@
-function getAccumulatorPoints(points){
+function getAccumulatorPoints(points) {
   return {"type": "FeatureCollection", "features": getAccumulatorArray(points, "points")};
-};
+}
 
-function getAccumulatorLine(points){
+function getAccumulatorLine(points) {
   var pointsArray = getAccumulatorArray(points, "line");
-  return {"type": "LineString", "coordinates": pointsArray.map(function(item){ return item.geometry.coordinates })};
-};
+  return {"type": "LineString", "coordinates": pointsArray.map(function (item){ return item.geometry.coordinates; })};
+}
 
-function getAccumulatorArray(points, option){
+function getAccumulatorArray(points, option) {
   var step, centroid, pointsArray;
   var centroidArray = [];
   pointsArray = [points[0], points[1]]; // Because we start w/ step 3.
-  for(step = 2; step < points.length; step++){
+  for (step = 2; step < points.length; step++) {
     pointsArray.push(points[step]);
     centroid = turf.centroid({"type": "FeatureCollection", "features": pointsArray});
-    if(option === "points"){
+    if (option === "points") {
       centroid.popup = "Text: " + points[step].properties.text + "; page: " + points[step].properties.page;
     };
     centroidArray.push(centroid);
   };
   return centroidArray;
-};
+}
 
-  
+function getRunningPoints(points) {
+  return {"type": "FeatureCollection", "features": getRunningArray(points, "points")};
+}
 
+function getRunningLine(points) {
+  var pointsArray = getRunningArray(points, "line");
+  return {"type": "LineString", "coordinates": pointsArray.map(function (item){ return item.geometry.coordinates; })};
+}
 
+function getRunningArray(points, option) {
+  var step, centroid;
+  var centroidArray = [];
+  for (step = 2; step < (points.length - 2); step++) {
+    runningArray = [points[step - 2], points[step -1], points[step], points[step + 1], points[step + 2]];
+    centroid = turf.centroid({"type": "FeatureCollection", "features": runningArray});
+    if (option === "points") {
+      centroid.popup = "Text: " + points[step].properties.text + "; page: " + points[step].properties.page;
+    };
+    centroidArray.push(centroid);
+  };
+  return centroidArray;
+}
 
-function getSpecialPoints(points, specialText){
+function getSpecialPoints(points, specialText) {
    var returnPoints = [];
-   points.features.forEach(function(item, index){
-     if(item.properties.special === specialText){
+   points.features.forEach(function (item, index) {
+     if (item.properties.special === specialText) {
        returnPoints.push(item);
      }
    });
